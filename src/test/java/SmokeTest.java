65e2104dc55a0bd379c6107b45cd461930db07fd
me <vilshinsky@gmail.com>
2015-11-10 15:39:29 +0200

    allure exploration

--- src/test/java/SmokeTest.java
+++ src/test/java/SmokeTest.java
@@ -3,32 +3,8 @@ import company.LogInPage;
 import company.ProfilePage;
 import company.TestHelper;
 import org.junit.*;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import ru.yandex.qatools.ashot.AShot;
 
-import java.util.Arrays;
-
-@RunWith(Parameterized.class)
 public class SmokeTest {
-    @Parameterized.Parameters
-    public static Iterable<Object[]> data() {
-        return Arrays.asList(new Object[][] {
-            { "1+2", new Integer(1) },
-            { "3*2", new Integer(3) }
-        });
-    }
-    private String input;
-    private double result;
-    public SmokeTest(String input, double result) {
-        this.input = input;
-        this.result = result;
-    }
-    @Test
-    public void test() {
-        Assert.assertEquals(result, );
-    }
-
     @BeforeClass
     public static void precondition() {
         TestHelper.setupChromeAndGo(Gigya.pageQAPGAT);
@@ -46,23 +22,10 @@ public class SmokeTest {
         TestHelper.quit();
     }
 
-    @Ignore
-    @Test
-    public void should() {
-        LogInPage.goToAuthorization();
-        LogInPage.passAuthorization();
-        ProfilePage.goToProfile();
-        new AShot().takeScreenshot(TestHelper.driver, TestHelper.waitElementByCss(".gigya-screen-dialog"));
-    }
-
     @Test
     public void shouldMatchLogInFormTexts() {
         LogInPage.goToAuthorization();
         Assert.assertEquals(LogInPage.authFormTexts(), LogInPage.getListAuthFormTexts());
-    }
-    @Test
-    public void galen() {
-
     }
 
     @Ignore
