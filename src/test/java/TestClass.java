da8ad88ce29b5cd354a1925f0b3586a7ae461541
Alexander <vilshinsky@gmail.com>
2015-11-30 14:03:19 +0200

    drone test class added

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ASmokeTest1.class,
+        SmokeTest.class
+})
+public class TestClass {
+}
